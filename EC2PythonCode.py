c7bd24cc342df34d419597761b846eae0a6e8e3e
Brian Jefferson <92610900+CushuN@users.noreply.github.com>
2023-06-07 14:06:57 -0500

    Update EC2PythonCode.py

--- EC2PythonCode.py
+++ EC2PythonCode.py
@@ -8,26 +8,26 @@ def generate_ec2_names(num_ec2instances, department_name):
     
     for _ in range(num_ec2instances):
         random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-        ec2_name = f"{department_name}-{random_chars}"
-        ec2_names.append(ec2_name)
+        ec2_name = f"{department_name}-{random_chars}"  # Generate EC2 name by combining department name and random characters
+        ec2_names.append(ec2_name)  # Add the generated name to the list of EC2 names
     
     return ec2_names
 
 def main():
-    num_ec2instances = int(input("How many EC2 instances do you want names for?: "))
-    department_name = input("What is the name of your department?: ")
+    num_ec2instances = int(input("How many EC2 instances do you want names for?: "))  # Prompt the user to enter the number of EC2 instances
+    department_name = input("What is the name of your department?: ")  # Prompt the user to enter the department name
     
-    department_name = department_name.lower().capitalize()
+    department_name = department_name.lower().capitalize()  # Convert the department name to lowercase and capitalize the first letter
     
-    if department_name in DEPARTMENTS_WITH_ACCESS:
-        ec2_names = generate_ec2_names(num_ec2instances, department_name)
+    if department_name in DEPARTMENTS_WITH_ACCESS:  # Check if the department name is in the list of departments with access
+        ec2_names = generate_ec2_names(num_ec2instances, department_name)  # Generate EC2 names based on the given number of instances and department name
         
         print("Generated EC2 names:")
         for name in ec2_names:
-            print(name)
+            print(name)  # Print each generated EC2 name
     else:
         print("That department can not use this Name Generator. Please choose from the following departments:")
-        print(", ".join(DEPARTMENTS_WITH_ACCESS))
+        print(", ".join(DEPARTMENTS_WITH_ACCESS))  # Print the list of departments with access
 
 if __name__ == "__main__":
-    main()
+    main()  # Call the main function if the script is run directly
